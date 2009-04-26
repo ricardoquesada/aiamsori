19cefab5365f3ec9bf191a88141cd6ae75d1d892
lwcyphr <lwcyphr@a18c5938-119a-11de-a46f-cd5bcf99231e>
2009-04-26 10:46:51 +0000

    player acceleration

--- trunk/gamelib/gamectrl.py
+++ trunk/gamelib/gamectrl.py
@@ -9,28 +9,21 @@ from pyglet.window import key
 class GameCtrl(Layer):
     is_event_handler = True
 
-    def __init__(self, main_layer):
+    def __init__(self, game_layer):
         super(GameCtrl, self).__init__()
-        self.main_layer = main_layer
+        self.game_layer = game_layer
         
-        self.pressed_keys = key.KeyStateHandler()
-        director.window.push_handlers(self.pressed_keys)        
+#        self.pressed_keys = key.KeyStateHandler()
+#        director.window.push_handlers(self.pressed_keys)        
 
     def on_key_press(self, k, m):
 
 
         if k in [key.UP, key.W]:
-#            print self.pressed_keys
-            print self.main_layer.player.speed
-            self.main_layer.player.speed = 300
-            print self.main_layer.player.speed
-
-##             if self.main_layer.player.speed < 300:
-##                 print self.main_layer.player.speed
-##                 self.main_layer.player.speed += 10
+            self.game_layer.player.acceleration= 2
 
         if k in [key.DOWN, key.S]:
-            self.main_layer.player.speed = -300
+            self.game_layer.player.acceleration= -2
 
         if k in [key.RIGHT, key.D]:
             pass
@@ -41,10 +34,10 @@ class GameCtrl(Layer):
 
     def on_key_release(self, k, m):
         if k in [key.UP, key.DOWN, key.W, key.S]:
-            print "release"
-            self.main_layer.player.speed = 0
+            self.game_layer.player.acceleration = 0
+            self.game_layer.player.speed = 0
 
     def on_mouse_motion(self, px, py, dx, dy):
-        self.main_layer.player.look_at(px, py)
+        self.game_layer.player.look_at(px, py)
             
 
