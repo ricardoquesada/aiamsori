b059cf8570dade0ed327a3e0dfb1c892094f51ac
lwcyphr <lwcyphr@a18c5938-119a-11de-a46f-cd5bcf99231e>
2009-04-26 05:47:10 +0000

    tiles y chars de prueba

--- gamectrl.py
+++ gamectrl.py
@@ -1,20 +1,36 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gamelib/'))
+
 from cocos.director import director
 from cocos.scene import Scene
 from cocos.layer import Layer, ColorLayer
+from cocos.sprite import Sprite
+
+from pyglet.window import key
 
 
 class GameCtrl(Layer):
     is_event_handler = True
 
-    def __init__(self):
+    def __init__(self, game):
         super(GameCtrl, self).__init__()
+        self.game = game
+
+    def on_key_press(self, k, m):
+        if k == key.UP:
+            print "UP"
+##             game.player_char.position
 
 
 class DummyGame(ColorLayer):
 
-    def __init__(self):
-        super(DummyGame, self).__init__()
-        
+    def __init__(self, r, g, b, a):
+        super(DummyGame, self).__init__(r,g,b,a)
+        self.player_char = Sprite('data/img/tipito.png')
+        self.player_char.position = (100,100)
+
+        self.add(self.player_char)
         
 
 if __name__ == "__main__":
@@ -23,5 +39,6 @@ if __name__ == "__main__":
 
 
     dummy_game =  Scene(DummyGame(200,100,100,255))
+    dummy_game.add(GameCtrl(dummy_game))
     director.run(dummy_game)
 
