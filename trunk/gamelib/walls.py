ab008385484f0141fe144418a120bb9829dbc402
lwcyphr <lwcyphr@a18c5938-119a-11de-a46f-cd5bcf99231e>
2009-05-02 14:58:37 +0000

    gameplay - spawn-points

--- trunk/gamelib/walls.py
+++ trunk/gamelib/walls.py
@@ -115,9 +115,8 @@ def create_wall_layer(layers):
 # si cambian esto, tienen que ejecutar este archivo para que arregle el atlas y la lista de coordenadas
 conf_walls =  {'newtiles/pared.png':'walls/pared_v.png',
                 'newtiles/ventana.png':'walls/ventana_v.png',
-#                'tiles/ventana.png':'tiles/ventana_v.png',
                 'newtiles/alambre.png':'walls/alambre_v.png',
-#                'tiles/alambre_v.png':'tiles/alambre_v.png',
+                'newtiles/alambre_rot.png':'walls/alambre_v.png',
             }
 if __name__ == "__main__":
     from tiless_editor.atlas import TextureAtlas
