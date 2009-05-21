729b3928650e8d36a6bb88f45926d429bf7812c0
ccanepacc <ccanepacc@a18c5938-119a-11de-a46f-cd5bcf99231e>
2009-05-21 22:52:08 +0000

    Milestone 1: Enflaquecido main, reorganizado codigo, introducido servicios con duracion session
    main ahora tiene solo la responsabilidades
    	startup: inicializar systemas y servicios que estaran disponibles durante toda la session.
    	lanzar la escenana inicial
    
    Reorganizacion de codigo:
    GameLayer y los compañeros de escena fueron a gamescene.py
    Lo que es intro u outro fué a intro_outro.py
    	
    
    Servicios:
    	Los servicios se llaman con
    	import gg
    	gg.services["s_xxxxxx"]()
    
    donde s_xxxxxx es el nombre del servicio.
    
    	Los servicios se registran durante el startup, en main, cuando se importa el modulo que declara el servicio.
    	Los servicios se declaran asi:
    
    	def xxxxxx():
    	...
    
    	
    	# publish services
    	import gg
    	gg.services["s_xxxxxx"] = xxxxxx
    
    Por una cuestion de sanidad se pide que sea valido llamar a cualquier servicio luego de terminar el starup.
    Para no tener que registrar informacion de parametros, se pide que no admitan parametros.
    En el codigo actual se usan para registrar creadores de instancias de escenas.
    
    r269

---
+++ gamelib/gg.py
@@ -0,0 +1,7 @@
+# globals. For sanity only add things that do not change from the end of startup
+# to the end of session.
+
+# available services (callables with no params) registers there;
+# convention : services['s_foo'] = foo
+services = {}
+
